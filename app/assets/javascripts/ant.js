086f46e54c29f255bc97c6ca579bc5afa041c7cc
Tyralion <piliaiev@gmail.com>
2016-05-11 15:01:20 +0500

    Мелкие правки

--- app/assets/javascripts/ant.js
+++ app/assets/javascripts/ant.js
@@ -1,8 +1,2 @@
 //= require ant/base
 //= require ant/default
-//= require ant/twitter
-//= require ant/instagram
-//= require ant/youtube
-//= require ant/coub
-//= require ant/vimeo
-//= require ant/script
